5d5b5466ad2f17bc7aa0974ffdc0eb22581c4f4a
Martin Staadecker <machst@live.ca>
2017-10-28 16:23:01 -0400

    Create outline for MCL navigator

---
+++ src/navigation/MyNavigator.java
@@ -0,0 +1,84 @@
+package navigation;
+
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.*;
+import lejos.robotics.chassis.Wheel;
+import lejos.robotics.chassis.WheeledChassis;
+import lejos.robotics.geometry.Line;
+import lejos.robotics.geometry.Rectangle;
+import lejos.robotics.localization.MCLPoseProvider;
+import lejos.robotics.mapping.LineMap;
+import lejos.robotics.navigation.DestinationUnreachableException;
+import lejos.robotics.navigation.MovePilot;
+import lejos.robotics.navigation.Navigator;
+import lejos.robotics.navigation.Waypoint;
+import lejos.robotics.pathfinding.Path;
+import lejos.robotics.pathfinding.ShortestPathFinder;
+import utils.Config;
+import utils.Logger;
+
+public class MyNavigator {
+
+    private static final String LOG_TAG = MyNavigator.class.getSimpleName();
+
+    //TODO : Set actual values
+    private static final double WHEEL_DIAMETER = 0;
+    private static final double WHEEL_OFFSET = 0;
+
+    //TODO : Test and understand values
+    private static final int NUMBER_OF_PARTICLES = 200;
+    private static final int MAP_BORDER = 10;
+
+    //TODO : Map actual surface
+    private static final Rectangle BOUNDING_RECTANGLE = new Rectangle(0, 0, 0, 0);
+    private static final Line[] LINES = new Line[]{
+            new Line(0, 0, 0, 0),
+            new Line(0, 0, 0, 0)
+    };
+
+    private static final LineMap map = new LineMap(LINES, BOUNDING_RECTANGLE);
+    private static final ShortestPathFinder pathFinder = new ShortestPathFinder(map);
+
+    private static MCLPoseProvider poseProvider;
+    private static Navigator navigator;
+
+    static {
+        MovePilot pilot = createMovePilot();
+
+        EV3UltrasonicSensor distanceSensor = new EV3UltrasonicSensor(Config.PORT_ULTRASONIC_SENSOR);
+        RangeFinder finder = new RangeFinderAdapter(distanceSensor.getDistanceMode());
+
+        RangeScanner scanner = new FixedRangeScanner(pilot, finder);
+
+        poseProvider = new MCLPoseProvider(pilot, scanner, map, NUMBER_OF_PARTICLES, MAP_BORDER);
+
+        navigator = new Navigator(pilot, poseProvider);
+    }
+
+    private static MovePilot createMovePilot() {
+        RegulatedMotor leftMotor = new EV3LargeRegulatedMotor(Config.PORT_MOTOR_LEFT);
+        RegulatedMotor rightMotor = new EV3LargeRegulatedMotor(Config.PORT_MOTOR_RIGHT);
+
+        Wheel leftWheel = WheeledChassis.modelWheel(leftMotor, WHEEL_DIAMETER).offset(WHEEL_OFFSET);
+        Wheel rightWheel = WheeledChassis.modelWheel(rightMotor, WHEEL_DIAMETER).offset(WHEEL_OFFSET);
+
+        WheeledChassis chassis = new WheeledChassis(new Wheel[]{leftWheel, rightWheel}, WheeledChassis.TYPE_DIFFERENTIAL);
+
+        return new MovePilot(chassis);
+    }
+
+    public static void goToDestination(Waypoint destination, boolean immediateReturn) {
+        try {
+            Path path = pathFinder.findRoute(poseProvider.getPose(), destination);
+
+            navigator.followPath(path);
+
+        } catch (DestinationUnreachableException e) {
+            Logger.log(Logger.typeError, LOG_TAG, "Destination unreachable");
+        }
+        if (!immediateReturn) {
+            navigator.waitForStop();
+        }
+    }
+}
