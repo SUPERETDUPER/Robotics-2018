68be44e597d2d20ee35394c85a62ee1e2e240252
Martin Staadecker <machst@live.ca>
2017-10-28 12:12:06 -0400

    Create basic chassis

---
+++ src/utils/Config.java
@@ -0,0 +1,10 @@
+package utils;
+
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.Port;
+
+public class Config {
+    //TODO : Confirm/assign real values
+    public static final Port PORT_MOTOR_LEFT = MotorPort.A;
+    public static final Port PORT_MOTOR_RIGHT = MotorPort.B;
+}
