ee8ce84d4d511342fac998ae96965a08cc7d45ae
mcintoshj <jonathan@jonathanmcintosh.ca>
2018-02-26 13:37:00 -0500

    Added arm and paddle classes and a couple of methods

---
+++ src/EV3/hardware/Paddle.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) [2018] [Jonathan McIntosh, Martin Staadecker, Ryan Zazo]
+ */
+
+package EV3.hardware;
+
+public class Paddle {
+
+    public void moveBlockOffConveyor(boolean immediateReturn) {
+
+    }
+
+    public void hitBlock(boolean immediateReturn) {
+
+    }
+}
