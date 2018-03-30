feadc5ff6a6a0f5d50b9175f2f0b512481b99a7d
Jonathan <jonathan@jonathanmcintosh.ca>
2018-03-30 16:16:01 -0400

    `Updated brain`

--- src/ev3/Brain.java
+++ src/ev3/Brain.java
@@ -7,33 +7,61 @@ package ev3;
 import common.logger.Logger;
 import ev3.navigation.Controller;
 import ev3.navigation.MapOperations;
+import ev3.robot.Paddle;
 import ev3.robot.Robot;
+import lejos.robotics.Color;
 
 import java.util.List;
 
 class Brain {
     private static final String LOG_TAG = Brain.class.getSimpleName();
+    private static int[] listFoodColor = new int[3];
+    private static int index = 0;
+    private static Robot robot;
 
     static void start(Robot robot) {
-
-        int [] listFoodColor = new int [3];
-
+        Brain.robot = robot;
         Controller.get().init(robot);
         Controller.get().getPose();
 
-        
-        MapOperations.goToContainerBottomLeft(Controller.get().getPose());
-        MapOperations.goToContainerBottomRight(Controller.get().getPose());
-        MapOperations.goToContainerTopLeft(Controller.get().getPose());
-        MapOperations.goToContainerTopRight(Controller.get().getPose());
+        robot.getArm().goToFoodIn(true);
 
-        MapOperations.goToTempRegBlue(Controller.get().getPose());
-        MapOperations.goToTempRegGreen(Controller.get().getPose());
-        MapOperations.goToTempRegYellow(Controller.get().getPose());
-        MapOperations.goToTempRegRed(Controller.get().getPose());
+        MapOperations.goToContainerBottomRight(Controller.get().getPose());
+        pickupFood(robot.getColorSensors().getColorContainer());
+        MapOperations.goToContainerBottomLeft(Controller.get().getPose());
+        pickupFood(robot.getColorSensors().getColorContainer());
+        MapOperations.goToContainerTopLeft(Controller.get().getPose());
+        pickupFood(robot.getColorSensors().getColorContainer());
+        MapOperations.goToContainerTopRight(Controller.get().getPose());
+        pickupFood(robot.getColorSensors().getColorContainer());
+
+        for (int i = 0; i < 3; i++) {
+            switch (listFoodColor[i]) {
+                case Color.BLUE:
+                    MapOperations.goToTempRegBlue(Controller.get().getPose());
+                    break;
+                case Color.GREEN:
+                    MapOperations.goToTempRegGreen(Controller.get().getPose());
+                    break;
+                case Color.YELLOW:
+                    MapOperations.goToTempRegYellow(Controller.get().getPose());
+                    break;
+                case Color.RED:
+                    MapOperations.goToTempRegRed(Controller.get().getPose());
+            }
+        }
+
 
         Controller.get().waitForStop();
 
         Logger.info(LOG_TAG, Controller.get().getPose().toString());
+    }
+
+    private static void pickupFood(int tempColor) {
+        if (tempColor != Color.NONE) {
+            listFoodColor[index] = tempColor;
+            index++;
+            robot.getPaddle().hitBlock(true);
+        }
     }
 }
