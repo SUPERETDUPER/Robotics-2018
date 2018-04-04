60d793517c7de5ce55dbc274e0284d0e314d4702
Martin Staadecker <machstg@gmail.com>
2018-04-04 17:49:03 -0400

    Changed to fixed speed vs percentage of max

--- src/ev3/navigation/NavigatorBuilder.java
+++ src/ev3/navigation/NavigatorBuilder.java
@@ -8,25 +8,26 @@ import common.mapping.SurfaceMap;
 import ev3.communication.ComManager;
 import ev3.localization.RobotPoseProvider;
 import lejos.robotics.chassis.Chassis;
+import lejos.robotics.navigation.ArcRotateMoveController;
 import lejos.robotics.navigation.MoveController;
 import lejos.robotics.navigation.Pose;
 
 public class NavigatorBuilder {
 
-    private static final double ANGULAR_ACCELERATION = 200;
+    private static final double ANGULAR_ACCELERATION = 150;
     private static final double LINEAR_ACCELERATION = 400;
-    private static final double ANGULAR_SPEED_PERCENT = 0.5;
-    private static final double LINEAR_SPEED_PERCENT = 0.5;
+    private static final double ANGULAR_SPEED = 100;
+    private static final double LINEAR_SPEED = 200;
 
     private static final Pose STARTING_POSE = new Pose(2152, 573, 180);
 
-    public static MoveController getMoveProvider(Chassis chassis){
+    public static ArcRotateMoveController getMoveProvider(Chassis chassis){
         MyMovePilot pilot = new MyMovePilot(chassis);
 
         pilot.setAngularAcceleration(ANGULAR_ACCELERATION);
         pilot.setLinearAcceleration(LINEAR_ACCELERATION);
-        pilot.setLinearSpeed(pilot.getMaxLinearSpeed() * LINEAR_SPEED_PERCENT);
-        pilot.setAngularSpeed(pilot.getMaxAngularSpeed() * ANGULAR_SPEED_PERCENT);
+        pilot.setLinearSpeed(LINEAR_SPEED);
+        pilot.setAngularSpeed(ANGULAR_SPEED);
 
         return pilot;
     }
